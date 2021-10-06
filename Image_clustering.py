3a1d40a42f82b03b457be5729263e6682ed641b7
Muhammad Stracey <strmuh001@gmail.com>
2021-10-06 15:11:58 +0200

    Remove unused data imports

--- Image_clustering.py
+++ Image_clustering.py
@@ -13,35 +13,19 @@ from sklearn.preprocessing import StandardScaler  # For standardizing data
 from sklearn.model_selection import train_test_split
 import joblib
 
-fullimageData = pd.read_csv('Full_sample_list.csv')
-imageData = pd.read_csv('Combined_set_1.csv')
-centre_as_cast = pd.read_csv('Centre_As_cast.csv')
-centre_homogenised_560 = pd.read_csv('Centre_homogenised_at_560_degrees.csv')
-edge_as_cast = pd.read_csv('Edge_As_cast.csv')
-centre_homogenised_580 = pd.read_csv('Centre_homogenised_at_580_degrees.csv')
-edge_homogenised_560 = pd.read_csv('Edge_homogenised_at_560_degrees.csv')
-edge_homogenised_580 = pd.read_csv('Edge_homogenised_at_580_degrees.csv')
-
-as_cast = pd.concat([edge_as_cast,centre_as_cast])
-as_cast.y = ['as_cast' for i in range(len(as_cast.y))]
-homogenised_560 = pd.concat([centre_homogenised_560,edge_homogenised_560])
-homogenised_560.y = ['homogenised_560' for i in range(len(homogenised_560.y))]
-homogenised_580 = pd.concat([centre_homogenised_580,edge_homogenised_580])
-homogenised_580.y = ['homogenised_580' for i in range(len(homogenised_580.y))]
-fullsetsimple = pd.concat([as_cast, homogenised_560, homogenised_580])
-centres = pd.concat([centre_as_cast,centre_homogenised_560,centre_homogenised_580])
-
-means = pd.DataFrame([centre_as_cast.mean(),centre_homogenised_560.mean(),edge_as_cast.mean(), centre_homogenised_580.mean(),
-                      edge_homogenised_560.mean(),edge_homogenised_580.mean()])
 class ImageAnalysis:
     def __init__(self, data):
         self.data = data
+
+    def box_plot(self,df,strt=114, stp=141):
+        df[df.columns[strt:stp]].plot(kind='box')
+        plt.suptitle("Box Plot for:" + df.iloc[1]['y'])
 
     def pairGrid(self, bgn, end):
         # Creates a list of pixel labels to plot
         t = ["P" + str(i) for i in range(bgn, end)]
         # Creates a grid using Seaborn's PairGrid()
-        plt.figure(1)
+
         grd_plt = sns.PairGrid(
             self.data,
             vars=t,
@@ -91,6 +75,7 @@ class ImageAnalysis:
             marker='x',
             s=200,
             c='red')
+        plt.suptitle("K-Means Clustering Plot for Pixel Intensities of 120 and 130")
         plt.show()
         return km
 
@@ -104,16 +89,21 @@ class ImageAnalysis:
         NLabels = len(set(y_data))
         plt.figure(4)
         plt.plot(pca.explained_variance_ratio_)
+        plt.axvline(12,color = 'red', linestyle = 'dashed')
+        plt.suptitle("Scree Plot for Full Dataset", fontsize = 18)
+        plt.xlabel("Component Number", fontsize=15)
+        plt.ylabel("Explained Variance", fontsize=15)
         plt.xlim([-5, 30])
         # Plots the projected data set on the first two principal components and colors by class
-        plt.figure(5)
-        sns.scatterplot(
-            x=trn_tf[:, 0],
-            y=trn_tf[:, 1],
-            style=y_data,
-            hue=y_data,
-            palette=colours[:NLabels])
+        # plt.figure(5)
+        # sns.scatterplot(
+        #     x=trn_tf[:, 0],
+        #     y=trn_tf[:, 1],
+        #     style=y_data,
+        #     hue=y_data,
+        #     palette=colours[:NLabels])
         print('pca score:',pca.score(StandardScaler().fit_transform(x_data)))
+        return pca
 
 if __name__ == "__main__":
     main()
